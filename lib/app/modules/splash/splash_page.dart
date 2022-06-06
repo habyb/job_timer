a15974302096997bebd2f23b61fd7f6b542d165e
Habyb Fernandes <habybfernandes@gmail.com>
2022-06-06 20:21:28 -0300

    added splash page construction

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:job_timer/app/core/ui/job_timer_icons.dart';
-
 class SplashPage extends StatelessWidget {
 
   const SplashPage({ Key? key }) : super(key: key);
@@ -8,19 +6,14 @@ class SplashPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Column(
-             children: [
-               Container(),
-               Padding(
-                 padding: const EdgeInsets.all(8.0),
-                 child: TextFormField(
-                   decoration: const InputDecoration(label: Text('Input XXX')),
-                 ),
-               ),
-               ElevatedButton(onPressed: () {}, child: Text('Botão')),
-               Icon(JobTimerIcons.ok_circled2, size: 50,)
-             ],
+           body: Container(
+             decoration: const BoxDecoration(
+               gradient: LinearGradient(colors: [
+                 Color(0XFF0092B9),
+                 Color(0XFF0167B2)
+               ])
+             ),
+             child: Center(child: Image.asset('assets/images/logo.png')),
            ),
        );
   }
