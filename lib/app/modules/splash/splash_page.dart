edd5dbaaafbb603f39ba697441063754f1048077
Habyb Fernandes <habybfernandes@gmail.com>
2022-06-06 19:27:56 -0300

    added theme configuration

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -8,7 +8,18 @@ class SplashPage extends StatelessWidget {
    Widget build(BuildContext context) {
        return Scaffold(
            appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
+           body: Column(
+             children: [
+               Container(),
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: TextFormField(
+                   decoration: const InputDecoration(label: Text('Input XXX')),
+                 ),
+               ),
+               ElevatedButton(onPressed: () {}, child: Text('Botão'))
+             ],
+           ),
        );
   }
 }
