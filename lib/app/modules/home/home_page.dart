a236e4192ba2e1313ed3e30151eea367b7a379c8
Habyb Fernandes <habybfernandes@gmail.com>
2022-06-08 05:42:01 -0300

    added first part of the home page screen

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_timer/app/modules/home/widgets/header_projects_menu.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -7,8 +8,54 @@ class HomePage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('Home'),),
-           body: Container(),
+         drawer: const Drawer(
+           child: SafeArea(child: ListTile(
+             title: Text('Sair'),
+             )),
+         ),
+           body: SafeArea(
+             child: CustomScrollView(
+               slivers: [
+                 const SliverAppBar(
+                   title: Text('Projetos'),
+                   expandedHeight: 100,
+                   toolbarHeight: 100,
+                   centerTitle: true,
+                   shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.vertical(
+                       bottom: Radius.circular(15)
+                     )
+                   ),
+                 ),
+                 SliverPersistentHeader(
+                  delegate: HeaderProjectsMenu(),
+                  pinned: true,
+                 ),
+                 SliverList(delegate: SliverChildListDelegate([
+                   Container(
+                     color: Colors.red,
+                     height: 200,
+                   ),
+                   Container(
+                     color: Colors.red,
+                     height: 200,
+                   ),
+                   Container(
+                     color: Colors.red,
+                     height: 200,
+                   ),Container(
+                     color: Colors.red,
+                     height: 200,
+                   ),
+                   Container(
+                     color: Colors.red,
+                     height: 200,
+                     child: const Text('Final'),
+                   ),
+                 ]))
+               ]
+             ),
+           ),
        );
   }
 }
